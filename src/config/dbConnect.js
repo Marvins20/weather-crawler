4ca8c0bcebfd3e0e61055ccb94ba2b51b9ed17e9
Marvins20 <marcusabrantes1337@gmail.com>
2024-09-24 20:20:17 -0300

    mensagem

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import nodemon from "nodemon";
 
 async function conectaNaDatabase(){
-    mongoose.connect(process.env.MONGO_CONECTION_STRING);
-asdasdasd
+    mongoose.connect(process.env.MONGO_CONECTION_STRING);dasd
     return mongoose.connection;
 }
+
+
 
 export default conectaNaDatabase;
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import weatherRoutes from "./weather-routes.js"
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("Weather Crawler"))
-
+    console.log("Hello World")
     app.use(express.json(), weatherRoutes)
 };
 
