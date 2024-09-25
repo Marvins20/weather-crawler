f61812042854c3be31defe7f029721302f3440de
Marvins20 <marcusabrantes1337@gmail.com>
2024-09-24 23:13:51 -0300

    novas funcionalidades

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import weatherRoutes from "./weather-routes.js"
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("Weather Crawler"))
-    console.log("Hello World")
     app.use(express.json(), weatherRoutes)
 };
 
--- src/routes/weather-routes.js
+++ src/routes/weather-routes.js
@@ -2,7 +2,7 @@ import express from "express"
 import WeatherController from "../api/controllers/weather-controller.js"
 
 const routes = express.Router();
-
+console.log("Hello World");
 routes.get("/weather/:city/:startDate?/:finalDate?", WeatherController.getForecast);
 routes.post("/weather/:city/:startDate/:finalDate", WeatherController.addForecast);
 
